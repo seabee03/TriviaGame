059fe857b3c6cdde1aba11dca0c29a8b92198be4
Seabee03 <34292383+seabee03@users.noreply.github.com>
2019-06-06 22:04:25 -0500

    added questions to the trivia game

---
+++ assets/javascript/app.js
@@ -0,0 +1,26 @@
+// Questions for the trivia game
+var questions = [{
+    question: "Which of the two NBA teams, are currently in the NBA Finals?",
+    answers: ["Cleveland Cavaliers vs. Denver Nuggets",
+        "San Antonio Spurs vs. Golden State Warriors",
+        "Golden State Warriors vs. Toronto Raptors",
+        "Los Angeles Lakers vs. Toronto Raptors"
+    ],
+    correctAnswer: "Golden State Warriors vs. Toronto Raptors"
+}, {
+    question: "Which team was the 2018 NBA Finals Championship?",
+    answers: ["Cleveland Cavaliers", "Golden State Warriors", "Milwaukee Bucks", "San Antonio Spurs"],
+    correctAnswer: "Golden State Warriors"
+}, {
+    question: "Which team has more wins, as of June 6th, 2019?",
+    answers: ["The series is tied 1-1", "Golden State Warriors lead the series 3-2",
+        "Toronto Raptors lead the series 3-1", "Toronto leads the series 2-1"
+    ],
+    correctAnswer: "Toronto leads the series 2-1"
+}, {
+    question: "Which team does the former San Antonio Spurs team member, Kawhi Leonard, play for?",
+    answers: ["He is still with the San Antonio Spurs", "Golden State Warriors", "Toronto Raptors",
+        "None, he is not in the finals"
+    ],
+    correctAnswer: "Toronto Raptors"
+}]
